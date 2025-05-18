77d43c2aa42e8ddc1093506750b511f436b18f8d
Ruby Mason <mason.r@northeastern.edu>
2025-05-18 12:00:34 -0400

    modules

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -1,100 +1,91 @@
+import ModuleControlButtons from "./ModuleControlButtons";
+import ModulesControls from "./ModulesControls"
+import LessonControlButtons from "./LessonControlButtons";
+import { ListGroup } from "react-bootstrap";
+import { BsGripVertical } from "react-icons/bs";
 export default function Modules() {
     return (
         <div id="wd-module-page">
-            <button>Collapse All</button>{' '}
-            <button>View Progress</button>{' '}
-            <select id="wd-select-one-publish-mode">
-                <option value="PUBLISH_SELECTED">Publish Selected</option>
-                <option value="UNPUBLISH_SELECTED">Unpublish Selected</option>
-                <option selected value="PUBLISH_ALL">
-                    Publish All</option>
-                <option value="UNPUBLISH_ALL">Unpublish All</option>
-            </select>{' '}
-            <button>+ Module</button>
-            
-            <ul id="wd-modules">
-                <li className="wd-module">
-                    <div className="wd-title">Week 1 - Intro to Web Development</div>
-                    <ul className="wd-lessons">
-                        <li className="wd-lesson">
-                            <span className="wd-title">LEARNING OBJECTIVES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Introduction to the course</li>
-                                <li className="wd-content-item">Learn what is Web Development</li>
-                            </ul>
-                        </li>
-                        <li className="wd-lesson">
-                            <span className="wd-title">READING</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Full Stack Developer - Chapter 1</li>
-                                <li className="wd-content-item">Full Stack Developer - Chapter 2</li>
-                            </ul>
-                        </li>
-                        <li className="wd-lesson">
-                            <span className="wd-title">SLIDES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Introduction to Web Development</li>
-                                <li className="wd-content-item">Creating a HTTP server</li>
-                                <li className="wd-content-item">Creating a React Application</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li className="wd-module">
-                    <div className="wd-title">Week 2 - Getting Set Up</div>
-                    <ul className="wd-lessons">
-                        <li className="wd-lesson">
-                            <span className="wd-title">LEARNING OBJECTIVES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Setting up environment</li>
-                                <li className="wd-content-item">Learning to write code</li>
-                            </ul>
-                        </li>
-                        <li className="wd-lesson">
-                            <span className="wd-title">READING</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Full Stack Developer - Chapter 3</li>
-                                <li className="wd-content-item">Full Stack Developer - Chapter 4</li>
-                            </ul>
-                        </li>
-                        <li className="wd-lesson">
-                            <span className="wd-title">SLIDES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Downloading the software</li>
-                                <li className="wd-content-item">Running software on your computer</li>
-                                <li className="wd-content-item">Writting in HTML</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li className="wd-module">
-                    <div className="wd-title">Week 3 - Creating a Website</div>
-                    <ul className="wd-lessons">
-                        <li className="wd-lesson">
-                            <span className="wd-title">LEARNING OBJECTIVES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Deploying a website</li>
-                                <li className="wd-content-item">Customizing with CSS</li>
-                            </ul>
-                        </li>
-                        <li className="wd-lesson">
-                            <span className="wd-title">READING</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Full Stack Developer - Chapter 5</li>
-                                <li className="wd-content-item">Full Stack Developer - Chapter 6</li>
-                            </ul>
-                        </li>
-                        <li className="wd-lesson">
-                            <span className="wd-title">SLIDES</span>
-                            <ul className="wd-content">
-                                <li className="wd-content-item">Setting up GitHub</li>
-                                <li className="wd-content-item">What is CSS?</li>
-                                <li className="wd-content-item">Designing a website</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+            <ModulesControls /><br /><br /><br /><br />
+            <ListGroup className="rounded-0" id="wd-modules">
+                <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
+                    <div className="wd-title p-3 ps-2 bg-secondary"> 
+                        <BsGripVertical className="me-2 fs-3" /> Week 1 - Intro to Web Development <ModuleControlButtons />
+                    </div>
+                        <ListGroup className="wd-lessons rounded-0">
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                LEARNING OBJECTIVES </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Introduction to the course </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Learn what is Web Development </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                READING </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Full Stack Developer - Chapter 1 </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Full Stack Developer - Chapter 2 </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                SLIDES </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Introduction to Web Development </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Creating a HTTP server </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Creating a React Application </ListGroup.Item>   
+                        </ListGroup>
+                </ListGroup.Item>
+                <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
+                    <div className="wd-title p-3 ps-2 bg-secondary"> Week 2 - Getting Set Up </div>
+                        <ListGroup className="wd-lessons rounded-0">
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                LEARNING OBJECTIVES </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Setting up environment </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Learning to write code </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                READING </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Full Stack Developer - Chapter 3 </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Full Stack Developer - Chapter 4 </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                SLIDES </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Downloading the software </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Running software on your computer </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Writting in HTML </ListGroup.Item>   
+                        </ListGroup>
+                </ListGroup.Item>
+                <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
+                    <div className="wd-title p-3 ps-2 bg-secondary"> Week 3 - Creating a Website </div>
+                        <ListGroup className="wd-lessons rounded-0">
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                LEARNING OBJECTIVES </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Deploying a website </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Customizing with CSS </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                READING </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Full Stack Developer - Chapter 5 </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Full Stack Developer - Chapter 6 </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                SLIDES </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Setting up GitHub </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                What is CSS? </ListGroup.Item>
+                            <ListGroup.Item className="wd-lesson p-3 ps-1">
+                                Designing a website </ListGroup.Item>   
+                        </ListGroup>
+                </ListGroup.Item>
+            </ListGroup>
         </div>
 );}
   
--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,104 +1,4 @@
-// import { Link } from "react-router-dom";
-// export default function Dashboard() {
-//     return (
-//         <div id="wd-dashboard">
-//         <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-//         <h2 id="wd-dashboard-published">Published Courses (8)</h2> <hr />
-//             <div id="wd-dashboard-courses">
-//                 <div className="wd-dashboard-course">
-//                     <Link to="/Kambaz/Courses/1234/Home" className="wd-dashboard-course-link" >
-//                         <img src="/images/reactjs.jpg" width={200} />
-//                         <div>
-//                             <h5> CS1234 React JS </h5>
-//                             <p className="wd-dashboard-course-title"> Full Stack software developer </p>
-//                             <button> Go </button>
-//                         </div>
-//                     </Link>
-//                 </div>
-//                 <br />
-//                 <div className="wd-dashboard-course">
-//                     <Link to="/Kambaz/Courses/4400/Home" className="wd-dashboard-course-link" >
-//                         <img src="/images/machinelearning.jpg" width={200} />
-//                         <div>
-//                             <h5> DS4400 Machine Learning and Data Mining 1 </h5>
-//                             <p className="wd-dashboard-course-title"> Learn core ML techniques and data mining tools </p>
-//                             <button> Go </button>
-//                         </div>
-//                     </Link>
-//                 </div>
-//                 <br />
-//                 <div className="wd-dashboard-course">
-//                     <Link to="/Kambaz/Courses/2331/Home" className="wd-dashboard-course-link" >
-//                         <img src="/images/linearalgebra.jpg" width={200} />
-//                         <div>
-//                             <h5> MATH2331 Linear Algebra </h5>
-//                             <p className="wd-dashboard-course-title"> Foundations of Linear Algebra concepts </p>
-//                             <button> Go </button>
-//                         </div>
-//                     </Link>
-//                 </div>
-//                 <br />
-//                 <div className="wd-dashboard-course">
-//                     <Link to="/Kambaz/Courses/4120/Home" className="wd-dashboard-course-link" >
-//                         <img src="/images/nlp.jpg" width={200} />
-//                         <div>
-//                             <h5> CS4120 Natural Language Processing </h5>
-//                             <p className="wd-dashboard-course-title"> Build language models and analyze text using NLP properties </p>
-//                             <button> Go </button>
-//                         </div>
-//                     </Link>
-//                 </div>
-//                 <br />
-//                 <div className="wd-dashboard-course">
-//                     <Link to="/Kambaz/Courses/5005/Home" className="wd-dashboard-course-link" >
-//                         <img src="/images/infoethics.jpg" width={200} />
-//                         <div>
-//                             <h5> PHIL5005 Information Ethics </h5>
-//                             <p className="wd-dashboard-course-title"> Explore privacy, fairness, and accountability in emerging technologies </p>
-//                             <button> Go </button>
-//                         </div>
-//                     </Link>
-//                 </div>
-//                 <br />
-//                 <div className="wd-dashboard-course">
-//                     <Link to="/Kambaz/Courses/4100/Home" className="wd-dashboard-course-link" >
-//                         <img src="/images/ai.jpg" width={200} />
-//                         <div>
-//                             <h5> CS4100 Artificial Intelligence </h5>
-//                             <p className="wd-dashboard-course-title"> Design intelligent systems using search, logic, planning, and learning algorithms </p>
-//                             <button> Go </button>
-//                         </div>
-//                     </Link>
-//                 </div>
-//                 <br />
-//                 <div className="wd-dashboard-course">
-//                     <Link to="/Kambaz/Courses/3200/Home" className="wd-dashboard-course-link" >
-//                         <img src="/images/database.jpg" width={200} />
-//                         <div>
-//                             <h5> CS3200 Database Design </h5>
-//                             <p className="wd-dashboard-course-title"> Learn how to manage databases and query for results </p>
-//                             <button> Go </button>
-//                         </div>
-//                     </Link>
-//                 </div>
-//                 <br />
-
-
-//                 <div className="wd-dashboard-course">
-//                     <Link to="/Kambaz/Courses/3520/Home" className="wd-dashboard-course-link" >
-//                         <img src="/images/c++.jpg" width={200} />
-//                         <div>
-//                             <h5> CS3520 Programming in C++ </h5>
-//                             <p className="wd-dashboard-course-title"> Learn programming in C++ for various applications </p>
-//                             <button> Go </button>
-//                         </div>
-//                     </Link>
-//                 </div>
-//             </div>
-//         </div>
-// );}
-
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 import { Row, Col, Card, Button,  } from "react-bootstrap";
 
 export default function Dashboard() {
@@ -109,7 +9,7 @@ export default function Dashboard() {
             <h2 id="wd-dashboard-published">Published Courses (8)</h2>
             <hr />
             <div id="wd-dashboard-courses">
-                <Row xs={1} md={5} className="g-4">
+                <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4 mt-4 mb-5">
                     <Col className="wd-dashboard-course" style={{ width: "300px" }}>
                         <Card>
                             <Link to="/Kambaz/Courses/1234/Home"
