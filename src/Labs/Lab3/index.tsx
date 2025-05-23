b2d8977d12c70b618b2ff92292a3b5953715ea48
Ruby Mason <mason.r@northeastern.edu>
2025-05-23 15:52:21 -0400

    3.4.4 Lab

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,3 +1,17 @@
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import ForLoops from "./ForLoops";
+import IfElse from "./IfElse";
+import ImpliedReturn from "./ImpliedReturn";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import SimpleArrays from "./SimpleArrays";
+import TemplateLiterals from "./TemplateLiterals";
+import TernaryOperator from "./TernaryOperator";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
 
@@ -7,6 +21,20 @@ export default function Lab3() {
             <h2>Lab 3</h2>
             <VariablesAndConstants />
             <VariableTypes />
+            <BooleanVariables />
+            <IfElse />
+            <TernaryOperator />
+            <ConditionalOutputIfElse />
+            <ConditionalOutputInline />
+            <LegacyFunctions />
+            <ArrowFunctions />
+            <ImpliedReturn />
+            <TemplateLiterals />
+            <SimpleArrays />
+            <ArrayIndexAndLength />
+            <AddingAndRemovingToFromArrays />
+            <ForLoops />
+            <MapFunction />
         </div>
 );}
   
