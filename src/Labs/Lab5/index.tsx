85cbe3a3ddecb18f01c804c234d5a8dea63bb1f3
Ruby Mason <mason.r@northeastern.edu>
2025-06-08 18:47:23 -0400

    finished lab 5

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -4,6 +4,10 @@ import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithArrays from "./WorkingWithArrays";
+import HttpClient from "./HttpClient";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
+
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 
 export default function Lab5() {
@@ -21,6 +25,9 @@ export default function Lab5() {
                 <QueryParameters />
                 <WorkingWithObjects />
                 <WorkingWithArrays />
+                <HttpClient />
+                <WorkingWithObjectsAsynchronously />
+                <WorkingWithArraysAsynchronously />
             </div>
         </Container>
     );
