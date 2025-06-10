585a61503f1f0e7487d71530377433410ae070d1
Ruby Mason <mason.r@northeastern.edu>
2025-06-10 17:33:17 -0400

    fix refresh issus

--- src/Kambaz/Account/ProtectedRoute.tsx
+++ src/Kambaz/Account/ProtectedRoute.tsx
@@ -11,13 +11,26 @@
 // }
 
 
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Navigate, useParams } from "react-router-dom";
+import { setEnrollments } from "../Enrollments/reducer";
+import * as enrollmentsClient from "../Enrollments/client";
 
 export default function ProtectedRoute({ children }: { children: any }) {
+    const dispatch = useDispatch();
     const { currentUser } = useSelector((state: any) => state.accountReducer);
     const enrollments = useSelector((state: any) => state.enrollmentsReducer.enrollments);
     const { cid } = useParams();
+
+    // fetch the enrollments so we can refresh page
+    useEffect(() => {
+        if (currentUser && enrollments.length === 0) {
+            enrollmentsClient.fetchUserEnrollments(currentUser._id)
+                .then(data => dispatch(setEnrollments(data)))
+                .catch(err => console.error("Failed to fetch enrollments", err));
+        }
+    }, [currentUser, enrollments, dispatch]);
 
     if (!currentUser) {
         return <Navigate to="/Kambaz/Account/Signin" />;
@@ -25,13 +38,13 @@ export default function ProtectedRoute({ children }: { children: any }) {
 
     // check enrollment
     if (cid) {
-        // if (!enrollments || enrollments.length === 0) {
-        //     return <div>Loading enrollments...</div>;
-        // }
+        if (!enrollments || enrollments.length === 0) {
+            return <div>Loading enrollments...</div>;
+        }
         const isEnrolled = enrollments.some((c: any) => c._id === cid);
 
         if (!isEnrolled) {
-        return <Navigate to="/Kambaz/Dashboard" />;
+            return <Navigate to="/Kambaz/Dashboard" />;
         }
     }
 
